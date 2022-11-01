f3a55070f26f1c9ac195d78314bdd23cfa7ac1f5
Maksim Yurepin <yurepin.maks@mail.ru>
2022-11-01 17:09:00 +0300

    First run

--- src/main/java/com/example/applicantEnrollment/ApplicantEnrollmentApplication.java
+++ src/main/java/com/example/applicantEnrollment/ApplicantEnrollmentApplication.java
@@ -2,8 +2,9 @@ package com.example.applicantEnrollment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class ApplicantEnrollmentApplication {
 
 	public static void main(String[] args) {
