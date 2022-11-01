0f731b2b8d953747e9d1fef73f1feaa409804185
Maksim Yurepin <yurepin.maks@mail.ru>
2022-11-01 16:43:10 +0300

    First spring commit

---
+++ src/main/java/com/example/applicantEnrollment/ApplicantEnrollmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.applicantEnrollment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicantEnrollmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicantEnrollmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/applicantEnrollment/ApplicantEnrollmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.applicantEnrollment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicantEnrollmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
